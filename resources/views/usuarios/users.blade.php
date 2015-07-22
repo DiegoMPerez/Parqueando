0eead9fe1facc6568cd1714798b8ba9b75a08caa
DiegoMPerez <diegoaspnet1618@gmail.com>
2015-07-21 22:17:02 -0500

    final

--- resources/views/usuarios/users.blade.php
+++ resources/views/usuarios/users.blade.php
@@ -3,41 +3,41 @@
 users
 @stop
 @section('content')
+<div class="panel-default">
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead>
+                <tr><th>Nombre de Usuario</th><th>Rol</th><th>Email</th><th>Nombres</th><th>Apellidos</th></tr>
+            </thead>
+            @if(isset($users))
+            <tbody>
 
-<div class="table-responsive">
-    <table class="table">
-        <thead>
-            <tr><th>Nombre de Usuario</th><th>Rol</th><th>Email</th><th>Nombres</th><th>Apellidos</th></tr>
-        </thead>
-        @if(isset($users))
-        <tbody>
-            
-            
-<!--//este es un comentario-->
-            @foreach($users as $user)
-            <tr><td>{!! $user->name !!}</td>
-                <td>{!! $user->rol->name !!}</td>
-                <td>{!! $user->email !!}</td>
-                <td>{!! $user->nombres !!}</td>
-                <td>{!! $user->apellidos  !!}</td>
 
-                <td>{!! Form::open(array('method' => 'GET', 'route' => array('usuarios.edit', $user->id))) !!}
-                    {!! Form::submit('Editar', array('class' => 'btn btn-info')) !!}
-                    {!! Form::close() !!}</td>
+                <!--//este es un comentario-->
+                @foreach($users as $user)
+                <tr><td>{!! $user->name !!}</td>
+                    <td>{!! $user->rol->name !!}</td>
+                    <td>{!! $user->email !!}</td>
+                    <td>{!! $user->nombres !!}</td>
+                    <td>{!! $user->apellidos  !!}</td>
 
-                <td>
-                    {!! Form::open(array('id' => 'form-eliminar','method' => 'DELETE', 'route' => array('usuarios.destroy', $user->id))) !!}
-                    {!! Form::button('Eliminar', array('class' => 'open btn btn-danger','id' => 'btn-eliminar','data-toggle' => 'modal', 'data-target' => '#myModal', 'data-username' => $user-> nombres, 'data-apellidos' => $user-> apellidos)) !!}
-                    {!! Form::close() !!}
-                </td>
+                    <td>{!! Form::open(array('method' => 'GET', 'route' => array('usuarios.edit', $user->id))) !!}
+                        {!! Form::submit('Editar', array('class' => 'btn btn-info')) !!}
+                        {!! Form::close() !!}</td>
 
-            </tr>
-            @endforeach
-        </tbody>
-        @endif
-    </table>
+                    <td>
+                        {!! Form::open(array('id' => 'form-eliminar','method' => 'DELETE', 'route' => array('usuarios.destroy', $user->id))) !!}
+                        {!! Form::button('Eliminar', array('class' => 'open btn btn-danger','id' => 'btn-eliminar','data-toggle' => 'modal', 'data-target' => '#myModal', 'data-username' => $user-> nombres, 'data-apellidos' => $user-> apellidos)) !!}
+                        {!! Form::close() !!}
+                    </td>
+
+                </tr>
+                @endforeach
+            </tbody>
+            @endif
+        </table>
+    </div>
 </div>
-
 
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
     <div class="modal-dialog" role="document">
