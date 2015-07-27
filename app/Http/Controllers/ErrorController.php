263568968925aef239954d4def4abc652d12c0fa
DiegoMPerez <diegoaspnet1618@gmail.com>
2015-07-27 09:17:59 -0500

    error 403

--- app/Http/Controllers/ErrorController.php
+++ app/Http/Controllers/ErrorController.php
@@ -1,17 +1,87 @@
-<?php
+<?php namespace App\Http\Controllers;
 
-namespace App\Http\Controllers;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+use Illuminate\Http\Request;
 
 class ErrorController extends Controller {
 
-    public function e403() {
-        return view('errors.403');
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index($id)
+	{
+            if($id === "403"){
+                return view('errors.403');
+            }
+            return view('errors.404');
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
 
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -39,9 +39,4 @@ Route::resource('/usuarios', 'UserController');
 
 
 
-Route::get('/error/{id}', function ($id) {
-    if ($id === "403") {
-        return view('errors.403');
-    }
-    return view('errors.404');
-});
+Route::get('/error/{id}',['uses' => 'ErrorController@index'] );
