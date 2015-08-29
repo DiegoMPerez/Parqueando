839c328820759df5cec91ff98ababe3cfb9b9a75
DiegoMPerez <diegoaspnet1618@gmail.com>
2015-08-29 15:45:22 -0500

    version

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -62,7 +62,7 @@
         <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
         @yield('linkbot')
         <div class="footer">
-            <p style="margin-right: 25px; margin-top: 5px; font-family: monospace; text-align: right"> v. 0.7.0 </p>
+            <p style="margin-right: 25px; margin-top: 5px; font-family: monospace; text-align: right"> v. 0.7.5 </p>
         </div>
     </body>
 </html>
