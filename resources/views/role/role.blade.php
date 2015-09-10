12de10db104674d21ab5069338305a5715757301
DiegoMPerez <diegoaspnet1618@gmail.com>
2015-09-09 23:17:22 -0500

    'multiple'

--- resources/views/role/role.blade.php
+++ resources/views/role/role.blade.php
@@ -3,19 +3,19 @@
 Roles
 @stop
 @section('linktop')
-{{ Html::style('css/multi-select.css') }}
-{{ Html::script('js/jquery.multi-select.js') }}
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<link href="{{ asset('/multiselectmaster/css/style.css') }}" rel="stylesheet"/>
+<script src="{{ asset('/multiselectmaster/js/multiselect.js') }}"></script>
 @stop
-
 @section('content')
 <div class="container">
     <div class="user">
         @if(Entrust::can('crear_roles'))
-                {{ Form::open(array('method' 
-                    => 'get', 'route' => array('roles.create'))) }}
-                    {{ Form::submit('Crear', array('class'
-                        => 'btn btn-success')) }}
-        {{ Form::close() }}
+        {!! Form::open(array('method' 
+        => 'get', 'route' => array('roles.create'))) !!}
+        {!! Form::submit('Crear', array('class'
+        => 'btn btn-success')) !!}
+        {!! Form::close() !!}
         @endif
         <table class="table table-bordered table-hover">
             <thead>
@@ -27,19 +27,19 @@ Roles
                 <tr><td>{{ $rol->name }}</td>
                     <td><a data-toggle="modal" rol_id="{{ $rol->id }}" data-target="#permisos" class="btn btn-primary get-permisos">Permisos</a></td>
                     @if(Entrust::can('editar_roles'))
-                                        <td>{{ Form::open(array('method' 
-                                        => 'GET', 'route' => array('roles.edit', $rol->id))) }}
-                                            {{ Form::submit('Editar', array('class'
-                        => 'btn btn-info')) }}
-                        {{ Form::close() }}</td>
+                    <td>{!! Form::open(array('method' 
+                        => 'GET', 'route' => array('roles.edit', $rol->id))) !!}
+                        {!! Form::submit('Editar', array('class'
+                        => 'btn btn-info')) !!}
+                        {!! Form::close() !!}</td>
                     @endif
                     @if(Entrust::can('eliminar_roles'))
                     <td>
-                                    {{ Form::open(array('method' 
-                                        => 'DELETE', 'route' => array('roles.destroy', $rol->id))) }}
-                                        {{ Form::submit('Eliminar', array('class'
-                                    => 'btn btn-danger')) }}
-                        {{ Form::close() }}
+                        {!! Form::open(array('method' 
+                        => 'DELETE', 'route' => array('roles.destroy', $rol->id))) !!}
+                        {!! Form::submit('Eliminar', array('class'
+                        => 'btn btn-danger')) !!}
+                        {!! Form::close() !!}
                     </td>
                     @endif
                 </tr>
@@ -56,13 +56,28 @@ Roles
                         <h4 class="modal-title">Gestionar permisos</h4>
                     </div>
                     <div class="modal-body">
-                        <select id="select-permisos" multiple="multiple">
-                            @if(isset($permisos))
-                            @foreach($permisos as $permiso)
-                            <option value="{{ $permiso->id }}">{{ $permiso->display_name }}</option>
-                            @endforeach
-                            @endif
-                        </select>
+                        <div class="row">
+                            <div class="col-xs-5">
+                                <select name="from[]" id="multiselect" class="form-control" size="8" multiple="multiple">
+                                    @if(isset($permisos))
+                                    @foreach($permisos as $permiso)
+                                    <option value="{{ $permiso->id }}">{{ $permiso->display_name }}</option>
+                                    @endforeach
+                                    @endif
+                                </select>
+                            </div>
+
+                            <div class="col-xs-2">
+                                <button type="button" id="multiselect_rightAll" class="btn btn-block"><i class="glyphicon glyphicon-forward"></i></button>
+                                <button type="button" id="multiselect_rightSelected" class="btn btn-block"><i class="glyphicon glyphicon-chevron-right"></i></button>
+                                <button type="button" id="multiselect_leftSelected" class="btn btn-block"><i class="glyphicon glyphicon-chevron-left"></i></button>
+                                <button type="button" id="multiselect_leftAll" class="btn btn-block"><i class="glyphicon glyphicon-backward"></i></button>
+                            </div>
+
+                            <div class="col-xs-5">
+                                <select name="to[]" id="multiselect_to" class="form-control" size="8" multiple="multiple"></select>
+                            </div>
+                        </div>
                     </div>
                     <div class="modal-footer">
                         <a href="#" data-dismiss="modal" class="btn">Cerrar</a>
@@ -73,13 +88,9 @@ Roles
 
     </div>
 </div>
-
-<style>
-    .user{
-        margin-top: 60px;
-    }
-</style>
-
-   
-
+<script type="text/javascript">
+jQuery(document).ready(function ($) {
+    $('#multiselect').multiselect();
+});
+</script>
 @stop
