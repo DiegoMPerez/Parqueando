cabfc8a8f6222da0f9e72f4fdf230541c96b06e1
DiegoMPerez <diegoaspnet1618@gmail.com>
2015-09-23 00:18:06 -0500

    Actualización de la referencia a fuente Google

--- resources/views/vehiculos/editar.blade.php
+++ resources/views/vehiculos/editar.blade.php
@@ -4,13 +4,12 @@ Editar Tipo Vehículos
 @stop
 
 @section('linktop')
-<link href="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
+
 <link href="{{ asset('/bootstrapfileinput/css/fileinput.css') }}" media="all" rel="stylesheet" type="text/css" />
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="{{ asset('/bootstrapfileinput/js/fileinput.js') }}" type="text/javascript"></script>
 <script src="{{ asset('/bootstrapfileinput/js/fileinput_locale_es.js') }}"></script>
 
-@endsection
+@stop
 
 @section('content')
 
