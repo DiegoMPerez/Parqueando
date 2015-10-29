d0365569e83ec8d0e946a08a3fcbe8ae1249d754
DiegoMPerez <diegoaspnet1618@gmail.com>
2015-10-29 01:11:24 -0500

    ruta iconos

--- resources/views/parqueando/parqueando.blade.php
+++ resources/views/parqueando/parqueando.blade.php
@@ -9,29 +9,35 @@ WEB PARQUEANDO
     <div class="panel-body" >
         <div class="row">
             <div class="col-md-4 col-sm-4 col-xs-12">
-                <div class="form-group">
-                    {!! Form::label('buscar', 'Buscar Lugar:', ['class' => 'col-md-2 col-xs-12 control-label']) !!}
-                    <div class="col-md-10 col-xs-12">
-                        {!! Form::text('buscar', '', ['class' => 'form-control']) !!}
+                <form class="form-login">
+                    <div class="form-group">
+                        {!! Form::label('buscar', 'Buscar Lugar:', ['class' => 'col-md-2 col-xs-12 control-label']) !!}
+                        <div class="col-md-10 col-xs-12">
+                            {!! Form::text('buscar', '', ['class' => 'form-control']) !!}
+                        </div>
                     </div>
-                </div>
-                <div class="form-group text-center">
-                    {!! Form::label('buscar', 'Información', ['class' => 'col-md-12 control-label']) !!}
-                    <div class="col-md-12 panel-info" id="infor">
-                        <label>lkajsd</label>
+                    <div class="form-group text-center">
+                        {!! Form::label('buscar', 'Información', ['class' => 'col-md-12 control-label']) !!}
+                        <div class="col-md-12 panel-info" id="infor">
+                            <label>lkajsd</label>
 
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <div class="col-md-12 col-sm-12 col-xs-12">
-                        {!! link_to('parqueaderos',"IR", array("class" => "btn-success col-sm-4 col-xs-4 col-md-4 col-md-offset-4 col-sm-offset-4 col-xs-offset-4 text-center")) !!}
+                    <div class="form-group">
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+                            {!! link_to('parqueaderos',"IR", array("class" => "btn-success col-sm-4 col-xs-4 col-md-4 col-md-offset-4 col-sm-offset-4 col-xs-offset-4 text-center")) !!}
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <div class="panel-body"></div>
-                </div>
+                    <div class="form-group">
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+                            <button id="go" class="btn-success col-sm-4 col-xs-4 col-md-4 col-md-offset-4 col-sm-offset-4 col-xs-offset-4 text-center">go</button>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="panel-body"></div>
+                    </div>
 
-
+                </form>
             </div>
 
 
@@ -58,12 +64,16 @@ WEB PARQUEANDO
 <script>
     $(document).ready(function () {
 
+        if ("geolocation" in navigator) {
+            console.log("Geolocalización Activada");
+            geolocalizacion();
+        } else {
+            console.log("Geolocalización Desactivada");
+        }
+
         // GEOLOCALIZACIÓN
-        geolocalizacion();
         function geolocalizacion() {
             if (navigator.geolocation.getCurrentPosition(showPosition)) {
-            } else {
-                showPosition(null);
             }
         }
         var _lat = 0;
@@ -77,16 +87,28 @@ WEB PARQUEANDO
                 _lng = position.coords.longitude;
             }
 
+
+
+
+
+//
             var directionsService = new google.maps.DirectionsService();
             var geocoder = new google.maps.Geocoder();
-            var image = "{{asset('img/marcador_parquenado.png')}}";
-            
+            var image = "{{asset('img/carro.png')}}";
+
             var directionsDisplay = new google.maps.DirectionsRenderer();
-            
-            var map = new google.maps.Map(document.getElementById('map-canvas'), {
+
+            var mapOptions = {
                 center: {lat: _lat, lng: _lng},
-                zoom: 15
-            });
+                zoom: 15,
+            };
+            var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+            directionsDisplay.setMap(map);
+
+
+
+
+
             var searchBox = new google.maps.places.SearchBox(document.getElementById('buscar'));
 
             var marker = new google.maps.Marker({
@@ -97,17 +119,15 @@ WEB PARQUEANDO
                 map: map,
                 draggable: false,
                 icon: image
-            });    
-            
+            });
+
             var inicio = new google.maps.LatLng(_lat, _lng);
-            var fin = new google.maps.LatLng(0.3190745, -78.107557);
-            
-            //calcRoute(inicio, fin);
-            
+
+
             function maker_changed(marker) {
                 var lat = marker.getPosition().lat();
                 var lng = marker.getPosition().lng();
-                var latlng = new google.maps.LatLng(lat, lng);
+
             }
 
             $('#buscar').keydown(function (event) {
@@ -127,13 +147,7 @@ WEB PARQUEANDO
                         if (status === google.maps.GeocoderStatus.OK) {
                             map.setCenter(results[0].geometry.location);
                             marker.setPosition(results[0].geometry.location);
-                            var lat = marker.getPosition().lat();
-                            var lng = marker.getPosition().lng();
-                            var latlng = new google.maps.LatLng(lat, lng);
-                            $('#lat').val(lat);
-                            $('#lng').val(lng);
                             map.setZoom(16);
-                            geocodePosition(latlng);
                         } else {
                             console.log(status);
                             if (status === google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
@@ -148,36 +162,29 @@ WEB PARQUEANDO
                     console.log(e.message);
                 }
             });
-            google.maps.event.addListener(marker, 'mouseup', function () {
-                var lat = marker.getPosition().lat();
-                var lng = marker.getPosition().lng();
-                var latlng = new google.maps.LatLng(lat, lng);
-            });
+
             google.maps.event.addListener(marker, 'position_changed', function () {
                 maker_changed(marker);
             });
 
+            var inicio = new google.maps.LatLng(_lat, _lng);
+            var fin = new google.maps.LatLng(0.32285139103669863, -78.10747106931149);
             
+            var beachMarker = new google.maps.Marker({
+                    position: fin,
+                    map: map,
+                    icon: "{{asset('img/marcador_p.png')}}"
+                });
+
+            calcRoute(inicio, fin);
+
             function calcRoute(inicio, fin) {
-                
+                //marker.setMap(null);
+
                 //var end = new google.maps.LatLng(38.334818, -181.884886);
                 //var end = new google.maps.LatLng(0.32328013765715263, -78.10352295764159);
-                var beachMarker = new google.maps.Marker({
-                    position: inicio,
-                    map: map,
-                    icon: image
-                });
-                /* var startMarker = new google.maps.Marker({
-                 position: start,
-                 map: map,
-                 draggable: false,
-                 icon: "{{asset('img/car.png')}}"
-                 });
-                 var endMarker = new google.maps.Marker({
-                 position: end,
-                 map: map,
-                 draggable: false
-                 });*/
+                
+
 
                 var bounds = new google.maps.LatLngBounds();
                 bounds.extend(inicio);
@@ -201,52 +208,15 @@ WEB PARQUEANDO
                     }
                 });
             }
+
         }
 //AJAX
         $('#enviar').click(function (event) {
 
-            event.preventDefault();
-            var url = "{{URL::route('parqueaderos.store')}}";
-            var form = $('#form');
-            var data = form.serialize();
-            $.ajax({
-                url: url,
-                type: 'POST',
-                data: $(this).closest('form').serialize(),
-                success: function (data) {
-                    // Success...
-                    console.log(data);
 
-                    location.href = "{{URL('parqueaderos/success')}}";
-                },
-                error: function (jqXhr) {
-                    if (jqXhr.status === 401) //redirect if not authenticated user.
-                        $(location).prop('pathname', 'auth/login');
-                    if (jqXhr.status === 422) {
-                        //process validation errors here.
-                        var errors = jqXhr.responseJSON; //this will get the errors response data.
-                        //show them somewhere in the markup
-                        //e.g
-                        errorsHtml = '<div class="alert alert-danger"><ul>';
-                        $.each(errors, function (key, value) {
-                            errorsHtml += '<li>' + value[0] + '</li>'; //showing only the first error.
-                        });
-                        errorsHtml += '</ul></di>';
-                        $('#form-errors').html(errorsHtml); //appending to a <div id="form-errors"></div> inside form
-                        $('html, body').animate({scrollTop: 0}, 'fast');
-                    }
-                    if (jqXhr.status === 403) {
-                        location.href = "{{URL('error403')}}";
-                    }
-                    if (jqXhr.status === 500) {
-                        location.href = "{{URL('error500')}}";
-                    } else {
 
-                    }
-                }
-            });
         });
-        
+
     });
 
 </script>
